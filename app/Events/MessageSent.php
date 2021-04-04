9533ef16aaca08498e56bceca69cea3ee6d9e2ed
Vitaliy Karakushan <karakushan@gmail.com>
2021-04-04 13:40:09 +0300

    fix chat

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -52,6 +52,6 @@ class MessageSent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('chat');
+        return new PresenceChannel('chat');
     }
 }
